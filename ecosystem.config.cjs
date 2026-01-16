e08d218bb33ef19331be006e5420d15fecf80966
cee <ceeyang@xone.la>
2026-01-16 17:39:36 +0700

    fix:  解决 pm2 一键启动脚本路由失效的问题

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -20,12 +20,10 @@ module.exports = {
         {
             name: "mini-mall-frontend",
             cwd: "./frontend",
-            script: "serve",
+            script: "npm",
+            args: "start",
             env: {
-                PM2_SERVE_PATH: '.',
-                PM2_SERVE_PORT: 3000,
-                PM2_SERVE_SPA: 'true',
-                PM2_SERVE_HOMEPAGE: '/index.html'
+                NODE_ENV: "production"
             }
         }
     ]
